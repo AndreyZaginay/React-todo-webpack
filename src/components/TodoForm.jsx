a6e77780bf6fbd67d00a94f87c86a38e99b1fc1a
AndreyZaginay <andrey201223@ukr.net>
2023-05-21 18:26:35 +0300

    add logic to todoform component

--- src/components/TodoForm.jsx
+++ src/components/TodoForm.jsx
@@ -1,9 +1,36 @@
-import React from 'react'
-import { MyInput } from './UI/Input/MyInput'
+import React from 'react';
 
-export const TodoForm = () => {
-  return (
-    // <MyInput/>
-    <div></div>
-  )
+import { MyInput } from './UI/Input/MyInput';
+
+export const TodoForm = ({ create }) => {
+    const [todo, setTodo] = useState({ title: '', body: ''});
+
+    const addNewTodo = (e)  => {
+      e.preventDefault();
+      const newTodo = {
+        ...todo, id: Date.now()
+      }
+      create(newTodo);
+      setTodo({title: '', body: ''});
+    }
+  
+    return(
+        <div>    
+            <form>
+                <MyInput 
+                    value={todo.title}
+                    onChange={e => setTodo({...todo, title: e.target.value})}
+                    type="text" 
+                    placeholder="Todo title..."
+                />
+                <MyInput 
+                    value={todo.body}
+                    onChange={e => setTodo({...todo, body: e.target.value})}
+                    type="text"
+                    placeholder="Todos description..."
+                />
+                <MyButton onClick={addNewTodo}>Add todo</MyButton>
+            </form>
+        </div>
+      )
 }
